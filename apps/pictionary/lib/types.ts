b73b84336951d9ff419fe885e52cf583b2d6ef04
svengal03 <saikumarvengali98@gmail.com>
2026-05-13 22:49:44 -0500

    fix: difficulty pictionary

--- apps/pictionary/lib/types.ts
+++ apps/pictionary/lib/types.ts
@@ -12,7 +12,8 @@ export interface Team {
   drawerIdx: number;
 }
 
-export type { Difficulty } from "@playhub/core";
+import type { Difficulty } from "@playhub/core";
+export type { Difficulty };
 
 export interface GameState {
   phase: Phase;
